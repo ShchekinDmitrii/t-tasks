76a089fdb645f6ecad4650c7de691a7b9ade03b1
Alexey Shchekin <shchekin.alexey@gmail.com>
2024-09-07 10:14:50 +0200

    Leap year task

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # t-tasks
 Tasks and solutions for T-Generation-2024
+
+"leap_year.py":
+Solution for the leap year problem
---
+++ leap_year.py
@@ -0,0 +1,28 @@
+d1,m1,y1 = input().split('.')
+d2,m2,y2 = input().split('.')
+
+day1 = int(d1)
+month1 = int(m1)
+year1 = int(y1)
+
+day2 = int(d2)
+month2 = int(m2)
+year2 = int(y2)
+
+years_passed = year2 - year1
+birthday2 = years_passed
+if (month2 > month1 or (month2 == month1 and day2 >= day1)):
+    birthday2 += 1
+
+N1 = years_passed // 400
+N2 = years_passed // 4
+N3 = years_passed // 100
+
+birthday1 = N1 + N2 - N3
+if (month2 > 2 or (month2==2 and day2==29)):
+    birthday1 += 1
+
+if (day1 == 29 and month1 == 2):
+    birthday2 = birthday1
+
+print(birthday1, birthday2)
